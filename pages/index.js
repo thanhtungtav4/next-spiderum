a2dbdf71b5a6ab0446d8b8706cc3b3022ba8a9bf
tungnt <nguyenthanhtung74119@gmail.com>
2021-08-29 23:18:34 +0700

    tungt

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
 import Layout from "../components/layout/layout";
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
     return ( 
