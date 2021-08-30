fc81c6515d4e69a80a659ce19fb109cac8a4ec70
tungnt <nguyenthanhtung74119@gmail.com>
2021-08-30 23:12:34 +0700

    add menu toggel

--- components/layout/header.js
+++ components/layout/header.js
@@ -2,6 +2,7 @@ import React from "react";
 import Image from 'next/image';
 import Link from 'next/link';
 import HeadMeta from "../module/HeadMeta";
+import MenuTopBottom from "../module/MenuTop";
 
 
 class Header extends React.Component {
@@ -17,36 +18,21 @@ class Header extends React.Component {
       <header className="c-header">
         <div className="c-header__inner">
           <div className="c-header__navTop">
-            <div className="c-header__logo"><a className="c-header__img" href="/"><img className="m-onlypc" src="assets/images/logo.png" alt="logo alt" /><img className="m-onlysp" src="assets/images/logosp.png" alt="logo alt" /></a></div>
+            <div className="c-header__logo">
+              <a className="c-header__img" href="/">
+                <img className="m-onlypc" src="assets/images/logo.png" alt="logo alt" />
+                <img className="m-onlysp" src="assets/images/logosp.png" alt="logo alt" />
+              </a>
+            </div>
             <ul className="c-header__icon">
               <li><a href="#"><i className="gg-search" /></a></li>
               <li><a href="/"><i className="gg-mail" /></a></li>
               <li><a href="/"><i className="gg-sun" /></a></li>
             </ul>
           </div>
-          <div>
-    </div>
-          <div className="c-header__navBottom">         
-            <div className={this.state.show ? 'c-header__btn is_active' : 'c-header__btn'} onClick={this.toggle}> <span><i className="gg-album" /></span><strong> CÁC CHỦ ĐỀ </strong><span> <i className="gg-chevron-down" /></span></div>
-            <div className="c-header__btn m-onlysp" id="navicon2"><span><i className="gg-loadbar-doc" /></span><strong>CẬP NHẬT</strong><span> <i className="gg-chevron-down"> </i></span></div>
-            <ul className="c-header__menu" id="siteNav2">
-              <li>  <Link href="/x"><a>Khoa học - Công nghệ</a></Link></li>
-              <li>  <Link href="/"><a>Truyền cảm hứng</a></Link></li>
-              <li>  <Link href="/"><a>Thể thao</a></Link></li>
-              <li>  <Link href="/"><a>Quan điểm - Tranh luận</a></Link></li>
-              <li>  <Link href="/"><a>Thời tiết</a></Link></li>
-              <li>  <Link href="/"><a>Công nghệ</a></Link></li>
-            </ul>
-          </div>
-          
-          <ul className={this.state.show ? 'c-header__togge is_active' : 'c-header__togge'}> 
-            <li> <Link href="/"><a> <Image src="https://picsum.photos/30/30" alt="" width={30} height={30}/><span>Khoa học - Công nghệ</span></a></Link></li>
-            <li> <Link href="/"><a> <Image src="https://picsum.photos/30/30" alt="" width={30} height={30}/><span>Khoa học - Công nghệ</span></a></Link></li>
-            <li> <Link href="/"><a> <Image src="https://picsum.photos/30/30" alt="" width={30} height={30}/><span>Khoa học - Công nghệ  </span></a></Link></li>
-            <li> <Link href="/"><a> <Image src="https://picsum.photos/30/30" alt="" width={30} height={30}/><span>Khoa học - Công nghệ</span></a></Link></li>
-            <li> <Link href="/"><a> <Image src="https://picsum.photos/30/30" alt="" width={30} height={30}/><span>Khoa học - Công nghệ</span></a></Link></li>
-            <li> <Link href="/"><a> <Image src="https://picsum.photos/30/30" alt="" width={30} height={30}/><span>Khoa học - Công nghệ</span></a></Link></li>
-          </ul>
+        <div>
+        </div>
+        <MenuTopBottom/>
         </div>
       </header>
     </>
---
+++ components/module/MenuTop.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import Image from 'next/image';
+import Link from 'next/link';
+function MenuTop(){
+    const [toggleState, setToggleState] = useState({
+        menuA: "",
+        menuB: ""
+    });
+
+    function toggle() {
+        setToggleState({
+            menuA: toggleState.menuA === "" ? "is_active" : "",
+            menuB: toggleState.menuB = ""
+        });
+    }
+    function toggle2() {
+        setToggleState({
+            menuB: toggleState.menuB === "" ? "is_active" : "",
+            menuA: toggleState.menuA = ""
+        });
+      }
+    return (
+      <>
+        <div className="c-header__navBottom">         
+            <div className={`c-header__btn ${toggleState.menuA}`} onClick={toggle}> <span><i className="gg-album" /></span><strong> CÁC CHỦ ĐỀ </strong><span> <i className="gg-chevron-down" /></span></div>
+            <div className={`c-header__btn m-onlysp ${toggleState.menuB}`} onClick={toggle2} ><span><i className="gg-loadbar-doc" /></span><strong>CẬP NHẬT</strong><span> <i className="gg-chevron-down"> </i></span></div>
+            <ul className={`c-header__menu ${toggleState.menuB}`}>
+              <li>  <Link href="/x"><a>Khoa học - Công nghệ</a></Link></li>
+              <li>  <Link href="/"><a>Truyền cảm hứng</a></Link></li>
+              <li>  <Link href="/"><a>Thể thao</a></Link></li>
+              <li>  <Link href="/"><a>Quan điểm - Tranh luận</a></Link></li>
+              <li>  <Link href="/"><a>Thời tiết</a></Link></li>
+              <li>  <Link href="/"><a>Công nghệ</a></Link></li>
+            </ul>
+        </div>
+        <ul className={`c-header__togge ${toggleState.menuA}`}> 
+            <li> <Link href="/"><a> <Image src="https://picsum.photos/30/30" alt="" width={30} height={30}/><span>Khoa học - Công nghệ</span></a></Link></li>
+            <li> <Link href="/"><a> <Image src="https://picsum.photos/30/30" alt="" width={30} height={30}/><span>Khoa học - Công nghệ</span></a></Link></li>
+            <li> <Link href="/"><a> <Image src="https://picsum.photos/30/30" alt="" width={30} height={30}/><span>Khoa học - Công nghệ  </span></a></Link></li>
+            <li> <Link href="/"><a> <Image src="https://picsum.photos/30/30" alt="" width={30} height={30}/><span>Khoa học - Công nghệ</span></a></Link></li>
+            <li> <Link href="/"><a> <Image src="https://picsum.photos/30/30" alt="" width={30} height={30}/><span>Khoa học - Công nghệ</span></a></Link></li>
+            <li> <Link href="/"><a> <Image src="https://picsum.photos/30/30" alt="" width={30} height={30}/><span>Khoa học - Công nghệ</span></a></Link></li>
+        </ul>
+        
+      </>
+    );
+}
+export default MenuTop;
